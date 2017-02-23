b7ddf8b792dbd0b573812e0cc3996efd729f3d62
Arjun Nagineni <arjun.nagineni@utexas.edu>
2017-02-22 19:43:48 -0600

    Changed text on sharing settings

--- js/tabs/schedule/SharingSettingsCommon.js
+++ js/tabs/schedule/SharingSettingsCommon.js
@@ -57,11 +57,11 @@ class SharingSettingsCommon extends React.Component {
         </Image>
         <View style={styles.content}>
           <Heading1 style={styles.h1}>
-            Let friends view your schedule in the F8 app?
+            Let friends view your schedule in the EWeek app?
           </Heading1>
           <Paragraph style={styles.p}>
-            This will not post to Facebook. Only friends using the F8 app will
-            be able to see your schedule in their My F8 tab.
+            This will not post to Facebook. Only friends using the EWeek app will
+            be able to see your schedule in their My EWeek tab.
           </Paragraph>
         </View>
       </View>
